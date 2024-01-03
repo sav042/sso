d3a750f293ca975bb4bc29690f85b4efb6615482
Винокуров Савелий <savinokurov@sbercloud.ru>
2024-01-03 14:53:13 +0300

    add config

---
+++ cmd/sso/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "sso/internal/config"
+
+func main() {
+	// init config
+	cfg := config.MustLoad()
+	// init logger
+	// init app
+	// init transport layer
+}
---
+++ internal/config/config.go
@@ -0,0 +1,56 @@
+package config
+
+import (
+	"flag"
+	"fmt"
+	"github.com/ilyakaznacheev/cleanenv"
+	"os"
+	"time"
+)
+
+type Config struct {
+	Env         string        `yaml:"env" env-default:"local"`
+	StoragePath string        `yaml:"storage_path" env-required:"true"`
+	TokenTTL    time.Duration `yaml:"token_ttl" env-required:"true"`
+	GRPC        GRPCConfig    `yaml:"grpc"`
+}
+
+type GRPCConfig struct {
+	Port    int           `yaml:"port"`
+	Timeout time.Duration `yaml:"timeout"`
+}
+
+func MustLoad() *Config {
+	configPath := fetchConfigPath()
+	if configPath == "" {
+		panic("config file is empty")
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		panic("config file does not exist: " + configPath)
+	}
+
+	var cfg Config
+	err := cleanenv.ReadConfig(configPath, &cfg)
+	if err != nil {
+		fmt.Printf("failed to read config file: " + err.Error())
+	}
+
+	return &cfg
+}
+
+// fetchConfigPath fetches config path from command line flag or environment variable.
+// Priority: flag > env > default.
+// Default value is empty string.
+func fetchConfigPath() string {
+	var res string
+
+	flag.StringVar(&res, "config", "", "path to config file")
+	flag.Parse()
+
+	if res == "" {
+		res = os.Getenv("CONFIG_PATH")
+	}
+
+	return res
+}
