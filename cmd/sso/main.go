278d005eb7645b9c6a2aab4c07c08a5222d00b67
Винокуров Савелий <savinokurov@sbercloud.ru>
2024-01-03 16:27:59 +0300

    create app, setup log

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,11 +1,52 @@
 package main
 
-import "sso/internal/config"
+import (
+	"log/slog"
+	"os"
+	"sso/internal/app"
+	"sso/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
 
 func main() {
 	// init config
 	cfg := config.MustLoad()
+
 	// init logger
+	log := setupLogger(cfg.Env)
+	if cfg.Env == envLocal {
+		log.Info("application config", slog.Any("cfg", cfg))
+	}
+
 	// init app
-	// init transport layer
+	application := app.New(log, cfg.GRPC.Port, cfg.StoragePath, cfg.TokenTTL)
+	application.GRPCSrv.MustRun()
+
+	// run server and apps
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
